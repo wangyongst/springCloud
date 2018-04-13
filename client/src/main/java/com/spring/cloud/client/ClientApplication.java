f3370bd25b150d31dcd6f761ea771d8ece3ba408
wangyongst <wangyongst@gmail.com>
2018-04-13 23:16:20 +0800

    home

--- client/src/main/java/com/spring/cloud/client/ClientApplication.java
+++ client/src/main/java/com/spring/cloud/client/ClientApplication.java
@@ -1,24 +1,27 @@
-package com.spring.cloud.server;
+package com.spring.cloud.client;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 @RestController
-public class ServiceApplication {
+public class ClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceApplication.class, args);
+        SpringApplication.run(ClientApplication.class, args);
     }
 
     @Value("${server.port}")
